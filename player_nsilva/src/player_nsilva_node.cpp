9bd8de2707021c9d26dc3f4a2bab83ab1551a508
Nuno Miguel Silva <nuno.miguel.silva@ua.pt>
2018-03-01 16:46:44 +0000

    my tf moves randomly 😈😈😈

--- player_nsilva/src/player_nsilva_node.cpp
+++ player_nsilva/src/player_nsilva_node.cpp
@@ -1,16 +1,31 @@
-#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <math.h>
+#include <ctime>
+
+// Boost includes
+#include <boost/shared_ptr.hpp>
+
+//ROS includes
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+//RWS includes
+#include <rws2018_libs/team.h>
+
 using namespace std;
 
+namespace rws_nsilva
+{
 class Player
 {
 public:
-  Player(std::string name)
+  Player(string name)
   {
     this->name = name;
   }
 
-  std::string name;
+  string name;
 
   void setTeamName(int index)
   {
@@ -24,7 +39,7 @@ public:
       setTeamName("none");
   }
 
-  void setTeamName(std::string team)
+  void setTeamName(string team)
   {
     if (team == "red" || team == "green" || team == "blue")
     {
@@ -34,34 +49,79 @@ public:
       cout << "[ERROR]: For player " + this->name + " team name " + team + " is invalid." << endl;
   }
 
-  std::string getTeamName(void)
+  string getTeamName(void)
   {
     return this->team_name;
   }
 
 private:
-  std::string team_name;
+  string team_name;
 };
 
 class MyPlayer : public Player
 {
 public:
-  MyPlayer(std::string argin_name, std::string argin_team) : Player(name)
+  boost::shared_ptr<Team> red_team;
+  boost::shared_ptr<Team> green_team;
+  boost::shared_ptr<Team> blue_team;
+  tf::TransformBroadcaster br;
+
+  double x, y;
+
+  MyPlayer(string argin_name, string argin_team) : Player(argin_name)
   {
+    red_team = boost::shared_ptr<Team>(new Team("red"));
+    green_team = boost::shared_ptr<Team>(new Team("green"));
+    blue_team = boost::shared_ptr<Team>(new Team("blue"));
+
+    x = y = 0.0;
+
     setTeamName(argin_team);
+
+    printReport();
+  }
+
+  void move()
+  {
+    // Send tf
+    tf::Transform transform;
+
+    //Update position
+    x += ((double)rand() / (RAND_MAX)) - 0.5;
+    y += ((double)rand() / (RAND_MAX)) - 0.5;
+
+    transform.setOrigin(tf::Vector3(x, y, 0.0));
+
+    tf::Quaternion q;
+    q.setRPY(0, 0, M_PI);
+    transform.setRotation(q);
+
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "nsilva"));
+  }
+
+  void printReport()
+  {
+    cout << "My name is " << name << ",my team is " << getTeamName() << " and I will catch you all >:)" endl;
   }
 };
+}
 
-int main()
+int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "nsilva");
+  ros::NodeHandle nh;
+  ros::Rate loop_rate(10);
+
   // Creating an instance of class Player
-  Player player("nsilva");
-  player.setTeamName("red");
+  rws_nsilva::MyPlayer my_player("nsilva", "blue");
 
-  std::cout << "Created an instance of class player with public name " << player.name << " in team "
-            << player.getTeamName() << std::endl;
+  while (ros::ok())
+  {
+    my_player.move();
 
-  MyPlayer my_player("nsilva", "red");
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
-  std::cout << "my_player team_name = " << my_player.getTeamName() << std::endl;
+  ros::spin();
 }
