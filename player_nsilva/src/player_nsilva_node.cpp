15b35f465ae30ac5a9ab50ad988eccce6f867573
Nuno Miguel Silva <nuno.miguel.silva@ua.pt>
2018-03-02 14:33:47 +0000

    marker text update ✍️

--- player_nsilva/src/player_nsilva_node.cpp
+++ player_nsilva/src/player_nsilva_node.cpp
@@ -191,20 +191,18 @@ public:
     marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
     marker.action = visualization_msgs::Marker::ADD;
     marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
+    marker.pose.position.y = -0.3;
     marker.pose.position.z = 0;
     marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.y = 0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
     marker.text = text;
-    marker.scale.x = 1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 1;
+    marker.scale.z = 0.3;
     marker.color.a = 1.0;  // Don't forget to set the alpha!
     marker.color.r = 0;
     marker.color.g = 0;
-    marker.color.b = 1.0;
+    marker.color.b = 1;
 
     vis_pub.publish(marker);
   }
