67250741581397558ac9d9f8f23692200e54ad9d
Nuno Miguel Silva <nuno.miguel.silva@ua.pt>
2018-03-02 12:26:38 +0000

    spinning round 🎡 🎡 🎡

--- player_nsilva/src/player_nsilva_node.cpp
+++ player_nsilva/src/player_nsilva_node.cpp
@@ -12,10 +12,11 @@
 
 // RWS includes
 #include <rws2018_libs/team.h>
+#include <rws2018_msgs/MakeAPlay.h>
 
 using namespace std;
 
-float randomizePosition(float* x, float* y)
+float randomizePosition(float *x, float *y)
 {
   srand(static_cast<unsigned>(time(0)));
   *x = (((static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) - 0.5) * 10);
@@ -79,17 +80,16 @@ public:
   tf::TransformBroadcaster br;
   tf::Transform transform;
 
+  boost::shared_ptr<ros::Subscriber> sub;
+  ros::NodeHandle nh;
+
   float x, y;
-  float vf;
 
   MyPlayer(string argin_name, string argin_team) : Player(argin_name)
   {
     red_team = boost::shared_ptr<Team>(new Team("red"));
     green_team = boost::shared_ptr<Team>(new Team("green"));
     blue_team = boost::shared_ptr<Team>(new Team("blue"));
-
-    vf = 0.0001;
-    randomizePosition(&x, &y);
 
     if (red_team->playerBelongsToTeam(name))
     {
@@ -110,6 +110,17 @@ public:
       my_hunters = green_team;
     }
 
+    struct timeval t1;
+    gettimeofday(&t1, NULL);
+    srand(t1.tv_usec);
+    double start_x = ((double)rand() / (double)RAND_MAX) * 10 - 5;
+    double start_y = ((double)rand() / (double)RAND_MAX) * 10 - 5;
+    printf("start_x=%f start_y=%f\n", start_x, start_y);
+    warp(start_x, start_y, M_PI / 2);
+
+    sub = boost::shared_ptr<ros::Subscriber>(new ros::Subscriber());
+    *sub = nh.subscribe("/make_a_play", 100, &MyPlayer::move, this);
+
     setTeamName(argin_team);
 
     printReport();
@@ -117,6 +128,8 @@ public:
 
   void warp(double x, double y, double alfa)
   {
+
+    transform.setOrigin(tf::Vector3(x, y, 0.0));
     transform.setOrigin(tf::Vector3(x, y, 0.0));
 
     tf::Quaternion q;
@@ -127,22 +140,46 @@ public:
     ROS_INFO("%s: Warping to x=%f, y=%f, alpha=%f", name.c_str(), x, y, alfa);
   }
 
-  void move()
+  void move(const rws2018_msgs::MakeAPlay::ConstPtr &msg)
   {
+    double x = transform.getOrigin().x();
+    double y = transform.getOrigin().y();
+    double a = 0;
+
+    // AI PART
+    double displacement = 6; // computed using AI
+    double delta_alpha = M_PI / 2;
+
+    // CONSTRAINSTS PART
+    double dist_max = msg->dog;
+    double dist_with_constrainsts;
+
+    if (displacement > dist_max)
+      displacement = dist_max;
+
+    double delta_alpha_max = M_PI / 3;
+    if (fabs(delta_alpha) > fabs(delta_alpha_max))
+      delta_alpha = delta_alpha_max * delta_alpha / fabs(delta_alpha);
+
+    tf::Transform my_move_transform;
+    my_move_transform.setOrigin(tf::Vector3(displacement, 0.0, 0.0));
+
     // Update position
-    int limit = 5;
-    if (x > limit || y > limit)
-      vf = -0.0001;
-    else if (x < -limit || y < -limit)
-      vf = 0.0001;
-    x += (((double)rand() / (RAND_MAX))) * vf;
-    y += (((double)rand() / (RAND_MAX))) * vf;
+    /*int limit = 5;
+        if (x > limit || y > limit)
+          vf = -1;
+        else if (x < -limit || y < -limit)
+          vf = 1;
+        x += vf;
+        y += vf;*/
 
     transform.setOrigin(tf::Vector3(x, y, 0.0));
 
     tf::Quaternion q;
-    q.setRPY(0, 0, M_PI);
-    transform.setRotation(q);
+    q.setRPY(0, 0, delta_alpha);
+    my_move_transform.setRotation(q);
+
+    transform = transform * my_move_transform;
 
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "nsilva"));
   }
@@ -154,7 +191,7 @@ public:
 };
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "nsilva");
   ros::NodeHandle nh;
@@ -163,13 +200,13 @@ int main(int argc, char** argv)
   // Creating an instance of class Player
   rws_nsilva::MyPlayer my_player("nsilva", "blue");
 
-  while (ros::ok())
+  /*while (ros::ok())
   {
     my_player.move();
 
     ros::spinOnce();
     loop_rate.sleep();
-  }
+  }*/
 
   ros::spin();
 }
